0ef2195ec70833b79e8146d4ad1ea65a52365747
patelscripts <navneetpatel325@gmail.com>
2026-08-07 22:25:29 +0530

    use groq api with google gemini api for quota limit

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -1,15 +1,34 @@
 import {google} from "@ai-sdk/google"
+import {groq} from "@ai-sdk/groq"
 import { convertToModelMessages, stepCountIs, streamText, UIMessage } from "ai"
 import {getReadme} from "@/lib/tools/getReadme"
 import { getFileStructure } from "@/lib/tools/getFileStructure";
 import { getRepoMetadata } from "@/lib/tools/getRepoMetadata";
 import { getOpenIssues } from "@/lib/tools/getOpenIssues";
 
+function isQuotaError(error : unknown) : boolean{
+  const messages = (error as Error)?.message?.toLowerCase() || "";
+  return messages.includes("quota") || messages.includes("429") || messages.includes("resouce_exhausted");
+}
+
 export async function POST(req : Request) {
     const { messages }: { messages: UIMessage[] } = await req.json();
 
+    let selectModel = google("gemini-2.5-flash");
+
+    try{
+      await streamText({
+        model : selectModel,
+        messages : [{role:"user", content:"ping"}]
+      }).text;
+    }catch(error){
+      if(isQuotaError(error)){
+        console.log("gemini quota has finished, now i am using groq");
+        selectModel = groq("llama-3.3-70b-versatile")
+      }
+    }
     const result = streamText({
-        model : google("gemini-2.0-flash"),
+        model : selectModel,
         system: `You are a helpful GitHub repository assistant. Your job is to help
       users understand any public GitHub repo — what it does, how it's structured,
       and how a new contributor could get started with it.
